4c0d18b05a0666951cac9045416883ef8433382e
Pratham Batra <pratham11.batra@gmail.com>
2025-04-02 22:19:17 +0530

    added functions to fetch data from api

--- client/app.js
+++ client/app.js
@@ -1,106 +1,88 @@
-// function getBathValue() {
-//     var uiBathrooms = document.getElementsByName("uiBathrooms");
-//     for(var i in uiBathrooms) {
-//       if(uiBathrooms[i].checked) {
-//           return parseInt(i)+1;
-//       }
-//     }
-//     return -1; // Invalid Value
-//   }
-  
-//   function getBHKValue() {
-//     var uiBHK = document.getElementsByName("uiBHK");
-//     for(var i in uiBHK) {
-//       if(uiBHK[i].checked) {
-//           return parseInt(i)+1;
-//       }
-//     }
-//     return -1; // Invalid Value
-//   }
-  
-//   function onClickedEstimatePrice() {
-//     console.log("Estimate price button clicked");
-//     var sqft = document.getElementById("uiSqft");
-//     var bhk = getBHKValue();
-//     var bathrooms = getBathValue();
-//     var location = document.getElementById("uiLocations");
-//     var estPrice = document.getElementById("uiEstimatedPrice");
-  
-//     // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-//     var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  
-//     $.post(url, {
-//         total_sqft: parseFloat(sqft.value),
-//         bhk: bhk,
-//         bath: bathrooms,
-//         location: location.value
-//     },function(data, status) {
-//         console.log(data.estimated_price);
-//         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-//         console.log(status);
-//     });
-//   }
-  
-//   function onPageLoad() {
-//     console.log( "document loaded" );
-//     // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-//     var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-//     $.get(url,function(data, status) {
-//         console.log("got response for get_location_names request");
-//         if(data) {
-//             var locations = data.locations;
-//             var uiLocations = document.getElementById("uiLocations");
-//             $('#uiLocations').empty();
-//             for(var i in locations) {
-//                 var opt = new Option(locations[i]);
-//                 $('#uiLocations').append(opt);
-//             }
-//         }
-//     });
-//   }
-  
-//   window.onload = onPageLoad;
+function onPageLoad() {
+  console.log("document loaded");
+  var url = "http://127.0.0.1:5000/get_location_names";
+  $.get(url, function (data, status) {
+    console.log("got response for get_location_names request");
+    if (data) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      uiLocations.innerHTML = "";
+      for (var i in locations) {
+        var opt = new Option(
+          toTitleCaseWithRomanNumerals(locations[i]),
+          locations[i]
+        );
+        uiLocations.appendChild(opt);
+      }
+    }
+  });
+}
 
 function getBathValue() {
-    let uiBathrooms = document.getElementsByName("uiBathrooms");
-    for (let i of uiBathrooms) {
-        if (i.checked) return i.value;
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for (var i in uiBathrooms) {
+    if (uiBathrooms[i].checked) {
+      return parseInt(i) + 1;
     }
-    return -1;
+  }
+  return -1; // Invalid Value
 }
 
 function getBHKValue() {
-    let uiBHK = document.getElementsByName("uiBHK");
-    for (let i of uiBHK) {
-        if (i.checked) return i.value;
+  var uiBHK = document.getElementsByName("uiBHK");
+  for (var i in uiBHK) {
+    if (uiBHK[i].checked) {
+      return parseInt(i) + 1;
     }
-    return -1;
+  }
+  return -1; // Invalid Value
 }
 
 function onClickedEstimatePrice() {
-    let sqft = document.getElementById("uiSqft").value;
-    let bhk = getBHKValue();
-    let bathrooms = getBathValue();
-    let location = document.getElementById("uiLocations").value;
-    let estPrice = document.getElementById("uiEstimatedPrice");
+  var sqft = document.getElementById("uiSqft");
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations");
+  var estPrice = document.getElementById("uiEstimatedPrice");
 
-    $.post("/api/predict_home_price", {
-        total_sqft: parseFloat(sqft),
-        bhk: parseInt(bhk),
-        bath: parseInt(bathrooms),
-        location: location
-    }, function (data) {
-        estPrice.innerHTML = `<h2>${data.estimated_price} Lakh</h2>`;
-    });
+  var url = "http://127.0.0.1:5000/predict_property_price";
+
+  $.post(
+    url,
+    {
+      total_sqft: parseFloat(sqft.value),
+      bhk: bhk,
+      bath: bathrooms,
+      location: location.value,
+    },
+    function (data, status) {
+      console.log(data.estimated_price);
+      estPrice.innerHTML =
+        "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+      console.log(status);
+    }
+  );
 }
 
-window.onload = function () {
-    $.get("/api/get_location_names", function (data) {
-        let locations = data.locations;
-        let uiLocations = document.getElementById("uiLocations");
-        locations.forEach(loc => {
-            let opt = new Option(loc, loc);
-            uiLocations.appendChild(opt);
-        });
-    });
-};
+function toTitleCaseWithRomanNumerals(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      let formattedWord = word.charAt(0).toUpperCase() + word.slice(1);
+
+      // Check if the word is a Roman numeral (matches I, II, III, IV, etc.)
+      if (
+        /^(i|ii|iii|iv|v|vi|vii|viii|ix|x|xi|xii|xiii|xiv|xv|xvi|xvii|xviii|xix|xx|xl|l|lx|lxx|lxxx|xc|c|d|m)$/i.test(
+          word
+        )
+      ) {
+        formattedWord = word.toUpperCase(); // Convert entire word to uppercase
+      }
+
+      return formattedWord;
+    })
+    .join(" ");
+}
+
+window.onload = onPageLoad;
