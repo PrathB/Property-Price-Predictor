bec5d2021bd934f5216cbaa5d6d868d28cfb60ca
Pratham Batra <pratham11.batra@gmail.com>
2025-04-03 18:31:55 +0530

    added code for debugging

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import util
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-CORS(app)
+app.config["DEBUG"] = False
+# If using CORS, configure it properly
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/get_location_names", methods=["GET"])
@@ -15,16 +19,26 @@ def get_location_names():
 
 @app.route("/predict_property_price", methods=["POST"])
 def predict_property_price():
-    total_sqft = float(request.form["total_sqft"])
-    location = request.form["location"]
-    bhk = int(request.form["bhk"])
-    bath = int(request.form["bath"])
+    try:
+        print(f"Request method: {request.method}")
+        print(f"Request form data: {request.form}")
+        total_sqft = float(request.form["total_sqft"])
+        location = request.form["location"]
+        bhk = int(request.form["bhk"])
+        bath = int(request.form["bath"])
 
-    response = jsonify(
-        {"estimated_price": util.get_estimated_price(location, total_sqft, bhk, bath)}
-    )
+        response = jsonify(
+            {
+                "estimated_price": util.get_estimated_price(
+                    location, total_sqft, bhk, bath
+                )
+            }
+        )
 
-    return response
+        return response
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":
