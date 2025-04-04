300d7ee0058cec26f99d1996284bd62978d9ce61
Pratham Batra <pratham11.batra@gmail.com>
2025-04-04 12:36:20 +0530

    minor fix

--- server/server.py
+++ server/server.py
@@ -4,20 +4,17 @@ import util
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.config["DEBUG"] = False
-# If using CORS, configure it properly
-CORS(app, resources={r"/*": {"origins": "*"}})
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/get_location_names", methods=["GET"])
 def get_location_names():
     response = jsonify({"locations": util.get_location_names()})
+    response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
 
 
-@app.route("/predict_property_price", methods=["POST"])
+@app.route("/predict_property_price", methods=["GET", "POST"])
 def predict_property_price():
     try:
         print(f"Request method: {request.method}")
@@ -34,6 +31,7 @@ def predict_property_price():
                 )
             }
         )
+        response.headers.add("Access-Control-Allow-Origin", "*")
 
         return response
     except Exception as e:
